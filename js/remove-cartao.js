0c4be88bcb6015f9b44043eb693661f8bef7940b
eviltadashi <iori.yagami.kof@gmail.com>
2018-11-13 22:20:36 -0200

    adicionado o for para varios botoes

--- js/remove-cartao.js
+++ js/remove-cartao.js
@@ -1,17 +1,14 @@
 
 (function(){
 
-    var btn = document.querySelector(".opcoesDoCartao-remove");
-
-    function removeCartao(){
-        var cartao = btn.parentNode.parentNode;
-        cartao.classList.add("cartao--some");
-        cartao.addEventListener("transitionend",function(){
-            cartao.remove();
-        })
-    }
-    btn.onclick = removeCartao;
-
+    const btns = document.querySelectorAll(".opcoesDoCartao-remove")
+        for(let i=0 ; i < btns.length ; i++){
+            btns[i].addEventListener('click',function(){
+                const cartao = btns[i].parentNode.parentNode
+                cartao.classList.add("cartao--some")
+                cartao.addEventListener("transitionend",function(){
+                    cartao.remove();
+                })
+            })
+        }
 })()
-
-
