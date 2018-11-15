be060beb3532d8f84d2a35d114bdf85893e2674f
Cleberson Pedreira Pauluci <pauluci.cleberson@gmail.com>
2018-11-14 22:49:29 -0200

    criados eventos nos cartoes

--- projeto-wd47-master/js/opcoesDoCartao/cartao.js
+++ projeto-wd47-master/js/opcoesDoCartao/cartao.js
@@ -3,6 +3,7 @@
     const cartoes = document.querySelectorAll(".cartao");
 
     cartoes.forEach(cartao => {
+        
         cartao.addEventListener("change", function(event) {
             const elementoHtml = event.target
             
@@ -14,6 +15,25 @@
                 cartao.style.backgroundColor = corEscolhida
             }
         })
+
+        cartao.addEventListener("focusin", () => {
+            cartao.classList.add("cartao--focado")
+        })
+
+        cartao.addEventListener("focusout", () => {
+            cartao.classList.remove("cartao--focado")
+        })
+
+        cartao.addEventListener("keydown", e => {
+            /* if (e.key === "Enter" || e.key === " ") {
+                e.target.click();
+            } */
+
+            if (e.code === "Enter" || e.code === "Space") {
+                e.target.click();
+            }
+        })
+
     })
 
 })()
