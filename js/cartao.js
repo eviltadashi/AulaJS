c4854ba613ba1f6503963bfdf4a44f31a0b5a9f7
eviltadashi <iori.yagami.kof@gmail.com>
2018-11-14 22:49:38 -0200

    fim da aula

--- js/cartao.js
+++ js/cartao.js
@@ -10,9 +10,44 @@
                 cartao.style.backgroundColor = elementoSelecionado.value
             }
       })
-    
-        
 
     })
 
 })()
+
+
+;(
+    function(){
+        const cartoes = document.querySelectorAll(".cartao")
+
+        cartoes.forEach(function(cartao){
+        
+           
+            cartao.addEventListener("focusin",function(){
+                cartao.classList.add("cartao--focado")
+            })
+
+            cartao.addEventListener("focusout",function(){
+                cartao.classList.remove("cartao--focado")
+            })
+        })
+    }
+)()
+
+
+;(
+    function(){
+        const cartoes = document.querySelectorAll(".cartao");
+
+        cartoes.forEach(function(cartao){
+            
+            cartao.addEventListener("keydown",function(e){
+                if(e.key==="enter" || e.key===" "){
+                    e.target.click()
+                }
+            })
+
+        })
+    }
+
+)()
